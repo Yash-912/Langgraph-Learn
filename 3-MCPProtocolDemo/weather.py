d10d5f0c1804171a17373f95a7dfdce8eb401743
Yash-912 <yashdoke861@gmail.com>
2026-07-18 21:00:54 +0530

    Testing MCP Severs in Langgraph & 2 transport protocols - HTTP and Stdio

---
+++ 3-MCPProtocolDemo/weather.py
@@ -0,0 +1,11 @@
+from mcp.server.fastmcp import FastMCP
+
+mcp = FastMCP("weather")
+
+@mcp.tool()
+async def get_weather(location: str)-> str:
+    """Get the weather location"""
+    return "ITs always raining in Mumbai"
+
+if __name__ =="__main__":
+    mcp.run(transport="streamable-http")
